de5f1efa2ced7d57786c73b74b09e88c54f26d47
mingfei <mingfei.net@gmail.com>
2017-01-05 11:44:42 +0800

    Update

--- day2/ddl.sql
+++ day2/ddl.sql
@@ -42,27 +42,44 @@ DESC day2.user; -- describe DESC 描述
 
 SHOW TABLES;
 
+-- entity
 CREATE TABLE student (
-  id         INT AUTO_INCREMENT PRIMARY KEY, -- 主关键字 主键 主码
-  name       VARCHAR(255) NOT NULL,
-  age        INT,
-  dob        DATE UNIQUE,
-  department VARCHAR(255)
+  id           INT AUTO_INCREMENT PRIMARY KEY, -- 主关键字 主键 主码
+  name         VARCHAR(255) NOT NULL,
+  age          INT,
+  dob          DATE UNIQUE,
+  departmentId INT,
+  CONSTRAINT FOREIGN KEY (departmentId) REFERENCES day2.department (id)
 );
 
 DROP TABLE day2.student;
 
 DESC day2.student;
 
-INSERT INTO day2.student VALUES (NULL , '', 18, NULL , 'CS'); -- null
-
-UPDATE day2.student
-SET department = 'EE'
-WHERE id = 3;
+INSERT INTO day2.student VALUES (NULL, 'Zhangsan', 18, '1999-1-1', 1); -- null
+INSERT INTO day2.student VALUES (NULL, 'Lisi', 19, '2000-1-1', 2); -- null
+INSERT INTO day2.student VALUES (NULL, 'Wanger', 20, '1997-1-1', 3); -- null
+INSERT INTO day2.student VALUES (NULL, 'tester', 20, '1996-1-1', 4); -- null
 
 SELECT *
 FROM day2.student;
 -- ORDER BY dob DESC ;
 
-DELETE FROM day2.student WHERE id = 2;
-DELETE FROM day2.student WHERE dob = '1990-1-2';
+DELETE FROM day2.student
+WHERE id = 2;
+DELETE FROM day2.student
+WHERE dob = '1990-1-2';
+
+-- table department entity
+CREATE TABLE day2.department (
+  id    INT AUTO_INCREMENT PRIMARY KEY,
+  dname VARCHAR(255) NOT NULL,
+  tel   VARCHAR(255)
+);
+
+SELECT *
+FROM day2.department;
+
+INSERT INTO day2.department VALUES (NULL, 'CS', '123');
+INSERT INTO day2.department VALUES (NULL, 'EE', '789');
+INSERT INTO day2.department VALUES (NULL, 'SS', '456');
