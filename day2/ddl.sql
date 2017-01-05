3a59ed576bd1bd1c2b01b6653e343c2e17c10adf
mingfei <mingfei.net@gmail.com>
2017-01-05 10:40:15 +0800

    Update

--- day2/ddl.sql
+++ day2/ddl.sql
@@ -21,7 +21,8 @@ CREATE TABLE day2.user (
   name   VARCHAR(255),
   height DOUBLE(3, 2),
   price  DECIMAL(6, 2),
-  dob DATE
+  dob    DATE,
+  time   DATETIME
 );
 
 SELECT *
@@ -33,7 +34,31 @@ UPDATE day2.user
 SET height = 1.70, name = 'tester'
 WHERE id = 1;
 
-INSERT INTO day2.user VALUES (NULL, 'zhangsan', 0.755, 0.755, '1990-1-2'); -- ?
+INSERT INTO day2.user VALUES (NULL, 'zhangsan', 0.755, 0.755, '1990-1-2', '2017-1-5 11:05:6'); -- ?
 SELECT sum(height)
 FROM day2.user;
 DESC day2.user; -- describe DESC 描述
+
+
+SHOW TABLES;
+
+CREATE TABLE student (
+  id         INT AUTO_INCREMENT PRIMARY KEY, -- 主关键字 主键 主码
+  name       VARCHAR(255),
+  age        INT,
+  dob        DATE,
+  department VARCHAR(255)
+);
+
+DROP TABLE day2.student;
+
+DESC day2.student;
+
+INSERT INTO day2.student VALUES (NULL, 'zhangsan', 18, '1990-1-1', 'CS');
+
+UPDATE day2.student
+SET department = 'EE'
+WHERE id = 3;
+
+SELECT *
+FROM day2.student;
