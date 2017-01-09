1caf0c08413ca6458463f315b967b8454fd8beb8
mingfei <mingfei.net@gmail.com>
2017-01-09 14:10:10 +0800

    Update

--- day3/dql.sql
+++ day3/dql.sql
@@ -22,3 +22,18 @@ WHERE id = 1;
 SELECT name, dob
 FROM db_student.student
 WHERE id = 2;
+
+SELECT DISTINCT gender -- 种类不同,\ 与,\ 种类
+FROM db_student.student;
+
+UPDATE db_student.student
+SET gender = 'Female'
+WHERE id = 2;
+
+SELECT *
+FROM db_student.student
+WHERE age = 18 AND gender = 'Male';
+
+SELECT *
+FROM db_student.student
+WHERE age = 18 OR gender = 'Male';
