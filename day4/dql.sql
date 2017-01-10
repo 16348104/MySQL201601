3b0338da5b97a113ad7fdbb7cee4e2ab31b7f0cd
mingfei <mingfei.net@gmail.com>
2017-01-10 10:06:14 +0800

    Update

--- day4/dql.sql
+++ day4/dql.sql
@@ -12,3 +12,91 @@ FROM scott.emp;
 SELECT *
 FROM scott.emp
 WHERE ENAME LIKE '_c%'; -- _ 数量为1的任意字符
+
+UPDATE scott.emp
+SET ENAME = 'S_COTT'
+WHERE ENAME = 'scott';
+
+SELECT *
+FROM scott.emp;
+
+SELECT *
+FROM scott.emp
+WHERE ENAME LIKE '%\_%' ESCAPE '\\';
+
+CREATE TABLE scott.emp_new LIKE scott.emp;
+
+SHOW TABLE STATUS FROM scott;
+
+DESC scott.emp;
+DESC scott.emp_new;
+
+SHOW CREATE TABLE scott.emp_new;
+
+CREATE TABLE `emp_new` (
+  `EMPNO`    INT(4) NOT NULL,
+  `ENAME`    VARCHAR(10)  DEFAULT NULL,
+  `JOB`      VARCHAR(9)   DEFAULT NULL,
+  `MGR`      INT(4)       DEFAULT NULL,
+  `HIREDATE` DATE         DEFAULT NULL,
+  `SAL`      DOUBLE(7, 2) DEFAULT NULL,
+  `COMM`     DOUBLE(7, 2) DEFAULT NULL,
+  `DEPTNO`   INT(2)       DEFAULT NULL,
+  PRIMARY KEY (`EMPNO`),
+  KEY `FK_emp_deptno` (`DEPTNO`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+SHOW CREATE TABLE scott.emp;
+
+CREATE TABLE `emp` (
+  `EMPNO`    INT(4) NOT NULL,
+  `ENAME`    VARCHAR(10)  DEFAULT NULL,
+  `JOB`      VARCHAR(9)   DEFAULT NULL,
+  `MGR`      INT(4)       DEFAULT NULL,
+  `HIREDATE` DATE         DEFAULT NULL,
+  `SAL`      DOUBLE(7, 2) DEFAULT NULL,
+  `COMM`     DOUBLE(7, 2) DEFAULT NULL,
+  `DEPTNO`   INT(2)       DEFAULT NULL,
+  PRIMARY KEY (`EMPNO`),
+  KEY `FK_emp_deptno` (`DEPTNO`),
+  CONSTRAINT `FK_emp_deptno` FOREIGN KEY (`DEPTNO`) REFERENCES `dept` (`DEPTNO`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+DROP TABLE scott.emp_new;
+
+SELECT *
+FROM scott.emp
+WHERE ENAME REGEXP 'a'; -- RLIKE REGEXP 正则匹配
+
+-- ===================== 正则表达式  REGEXP regular expression
+
+SELECT *
+FROM emp
+WHERE ENAME = BINARY 'WARd';
+
+SELECT *
+FROM scott.emp
+WHERE ENAME REGEXP 'a|b';
+
+SELECT *
+FROM emp
+WHERE ENAME LIKE '%a%' OR emp.ENAME LIKE '%b%';
+
+SELECT *
+FROM scott.emp
+WHERE ENAME REGEXP '[a-z]';
+
+UPDATE scott.emp
+    SET ENAME = '斯科特'
+WHERE EMPNO = 7788;
+
+SELECT *
+FROM scott.emp;
+
+SELECT *
+FROM emp
+WHERE ENAME NOT REGEXP '[\u4e00-\u9fbb]'; -- ?
