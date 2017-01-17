33c036920d457592719e1132ca287f512373ccfb
mingfei <mingfei.net@gmail.com>
2017-01-17 15:47:57 +0800

    Update

--- day8/user.sql
+++ day8/user.sql
@@ -14,6 +14,23 @@ CREATE TABLE db_user.user (
 )
   COMMENT '用户表';
 
+DROP TABLE IF EXISTS db_user.message;
+CREATE TABLE db_user.message (
+  id      INT AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'PK',
+  content TEXT NOT NULL
+  COMMENT '',
+  userId  INT COMMENT 'FK'
+)
+  COMMENT '留言表';
+
+-- FK
+ALTER TABLE db_user.message
+  ADD CONSTRAINT
+  fk_message_userId
+FOREIGN KEY (userId)
+REFERENCES db_user.user (id);
+
 -- 注册
 INSERT INTO db_user.user VALUES (NULL, 'tester@test.com', 'tester', '123');
 -- 登录
@@ -21,11 +38,14 @@ SELECT *
 FROM db_user.user
 WHERE email = 'tester@test.com' AND password = '123';
 -- 注销
+-- 留言
+INSERT INTO db_user.message VALUES (NULL, '...', 1);
+
 
 SELECT *
 FROM db_user.user;
 
-
-SHOW VARIABLES LIKE 'char%';
+SELECT *
+FROM db_user.message;
 
 
